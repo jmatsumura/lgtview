6bf2d27de0a4700a84edf97ac6d8e2e1bc11e084
James Matsumura <james.matsumura@gmail.com>
2016-06-22 10:12:58 -0400

    simplifying since the TwinBLAST interface is not needed to be as configurable as LGTview

--- bin/init_db.pl
+++ bin/init_db.pl
@@ -12,11 +12,6 @@ Sets up the database for the TwinBLAST user and populates the table with basic i
 
 This script uses MySQL but can be reconfigured to work with other DBs.
 
-It accepts a single argument which is the name of the database to be 
-created. Thus, this script can be run like so: 
-
-./init_db.pl my_db_name
-
 =head1 AUTHOR - James Matsumura
 
 e-mail: jmatsumura@som.umaryland.edu
@@ -26,21 +21,10 @@ e-mail: jmatsumura@som.umaryland.edu
 use strict;
 use DBI;
 
-
-# Extract MySQL connection info from a conf file
-my $chosen_db = $ARGV[1];
-
-my $dbh = DBI->connect("DBI:mysql:;host=MySQL",
-						"root", "");
-
-# First create the custom database
-$dbh->do("CREATE DATABASE ?", undef, $chosen_db);
-
-# Now grant permissions for the database
-$dbh->do("GRANT ALL on ?.* TO 'twinblast'\@'localhost'", undef, $chosen_db);
+my $dbh = DBI->connect("DBI:mysql:database=twinblast;host=MySQL",
+						"twinblast", "twinblast");
 
 # Populate the database with the two necessary tables for curation
-$dbh->do("use ?", undef, $chosen_db);
 $dbh->do("CREATE TABLE curation(seq_id VARCHAR(100), cv_id INT, PRIMARY KEY(seq_id))", undef);
 $dbh->do("CREATE TABLE cv(name VARCHAR(100), UNIQUE(name))", undef);
 $dbh->do("ALTER TABLE cv ADD COLUMN id INT NOT NULL AUTO_INCREMENT FIRST, ADD primary KEY id(id)", undef);
